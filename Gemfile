472a3ffde3d5eef304909c09c8f29bd7be0f306a
Kristoffer Darj <kristoffer.darj@gmail.com>
2016-10-21 22:25:28 +0200

    Started ruby guide

--- Gemfile
+++ Gemfile
@@ -9,13 +9,11 @@ gem 'uglifier', '>= 1.3.0'
 
 gem "pundit"
 
-#gem 'bcrypt', '~> 3.1.7'
-
-# Use Unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
+group :test do
+  gem "rspec-rails", "~> 2.14"
+  gem "factory_girl_rails"
+  gem 'ffaker'
+end
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,17 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+# don't generate RSpec tests for views and helpers
+  config.generators do |g|
+    g.test_framework :rspec, fixture: true
+    g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    g.view_specs false
+    g.helper_specs false
+    g.stylesheets = false
+    g.javascripts = false
+    g.helper = false
+  end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
