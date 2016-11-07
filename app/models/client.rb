15c9a54f59bfa0b32dca92abee8955f73c5ce301
Kristoffer Darj <kristoffer.darj@gmail.com>
2016-11-07 23:53:10 +0100

    fixing profile_id mockup

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,7 +3,7 @@ class Client < ActiveRecord::Base
   def self.get_profile client_id
     #just mocking something up
     @profile_data = {
-              profile_id: client_id * 2,
+              profile_id: client_id.to_i * 2,
               accuracy: 0.8
             }
   end
